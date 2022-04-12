de90b2a7f62a081aaaafef3d6f100485489c5561
Jiří Tribula <jiri.tribula@avast.com>
2022-04-12 21:42:13 +0200

    test aiohttp pydantic

--- src/applifting_exercise/web.py
+++ src/applifting_exercise/web.py
@@ -3,12 +3,35 @@ from importlib.metadata import version
 
 from aiohttp import web
 from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+from aiohttp_pydantic import PydanticView
+from pydantic import BaseModel
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(threadName)s] - %(levelname)-8s %(name)s: %(message)s",
 )
 LOGGER = logging.getLogger(__name__)
+
+
+class TestResponse(BaseModel):
+    test_string_1: str
+    test_string_2: str
+
+
+class TestRequest(BaseModel):
+    test_string: str
+
+
+class TestView(PydanticView):
+    async def get(self, test_request: TestRequest) -> Response:
+        test_string_1 = test_request.test_string
+
+        test_string_2 = self.request.app["test_dict"]["TEST"]
+
+        response = TestResponse(test_string_1=test_string_1, test_string_2=test_string_2)
+
+        return web.json_response(response.dict())
 
 
 class WebServer:
@@ -19,8 +42,12 @@ class WebServer:
         self.web_app = web.Application()
         self.runner = web.AppRunner(self.web_app)
 
+        self.test_dict = {"TEST": "TEST_STRING"}
+        self.web_app["test_dict"] = self.test_dict
+
     def _add_routes(self) -> None:
         self.web_app.router.add_route("GET", "/", self.get_all_routes)
+        self.web_app.router.add_route("GET", "/test", TestView)
 
     async def start_web_server(self) -> None:
         LOGGER.info("Start web server")
@@ -50,8 +77,10 @@ class WebServer:
         for route_item in routes_list:
             endpoint_method = route_item[1]
             endpoint_link = route_item[0]
-            html += f"<tr><td>{endpoint_method}</td><td style='padding-left:1em'>" \
-                    f"<a href='{endpoint_link}'>{endpoint_link}</a></td></tr>"
+            html += (
+                f"<tr><td>{endpoint_method}</td><td style='padding-left:1em'>"
+                f"<a href='{endpoint_link}'>{endpoint_link}</a></td></tr>"
+            )
 
         html += "</table>"
 
